69f83215c8c037454da55a2a896150262ba3ad23
Jay T <torrcorp@gmail.com>
2018-06-11 14:52:19 -0600

    created test case to iterate over ENUM values.

---
+++ test/edu/cnm/deepdive/cards/SuitTest.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive.cards;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuitTest {
+
+  @Test
+  void testToString() {
+    for (Suit suit : Suit.values()) {
+      //TODO Test suit.toString() for expected return value.
+    }
+  }
+}
