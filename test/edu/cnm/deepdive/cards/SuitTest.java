e258f99a6996938680bb4ae6d9c11c1530cc14ab
Jay T <torrcorp@gmail.com>
2018-06-12 10:20:12 -0600

    Created Deck Class and additional tests for class.

--- test/edu/cnm/deepdive/cards/SuitTest.java
+++ test/edu/cnm/deepdive/cards/SuitTest.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 
 class SuitTest {
 
+  private static final String[] SUIT_SYMBOLS = {
+      "\u2663",
+      "\u2666",
+      "\u2665",
+      "\u2660",
+  };
+
   @Test
   void testToString() {
     for (Suit suit : Suit.values()) {
-      //TODO Test suit.toString() for expected return value.
+      assertEquals(SUIT_SYMBOLS[suit.ordinal()], suit.toString());
+      System.out.println(suit);
     }
   }
 }
