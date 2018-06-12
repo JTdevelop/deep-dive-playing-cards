3de45ed7f46dc6148f407e51a935ced080892ac5
Jay T <torrcorp@gmail.com>
2018-06-12 15:56:54 -0600

    Add Console Game Class to implement player hand.

--- src/edu/cnm/deepdive/cards/BlackjackHand.java
+++ src/edu/cnm/deepdive/cards/BlackjackHand.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class BlackjackHand {
+public abstract class BlackjackHand implements Comparable<BlackjackHand> {
 
   private static final int[] VALUES = {
       1,
@@ -24,16 +24,25 @@ public class BlackjackHand {
   };
   private List<Card> hand;
   private Deck deck;
+  private boolean aceInHand;
+  private boolean soft;
+  private boolean blackjack;
+  private boolean busted;
+  private int value;
 
-  public BlackjackHand(Deck deck) throws InsufficientCardsException {
+  protected BlackjackHand(Deck deck) throws InsufficientCardsException {
     hand = new LinkedList<>();
     hand.addAll(Arrays.asList(deck.deal(2)));
     this.deck = deck;
+    update();
   }
 
-  private int value() {
-    boolean aceInHand = false;
-    int value = 0;
+  private void update() {
+    aceInHand = false;
+    value = 0;
+    soft = false;
+    busted = false;
+    blackjack = false;
     for (Card card : hand) {
       if (card.getRank() == Rank.ACE) {
         aceInHand = true;
@@ -42,9 +51,66 @@ public class BlackjackHand {
     }
     if (value > 21) {
       value = 0;
+      busted = true;
     } else if (value <= 11 && aceInHand) {
       value += 10;
+      soft = true;
+      if (hand.size() == 2 && value == 21) {
+        blackjack = true;
+      }
     }
+  }
+
+  protected final void hit() throws InsufficientCardsException {
+    hand.add(deck.deal());
+    update();
+  }
+
+  @Override
+  public int compareTo(BlackjackHand other) {
+    int comparison = Integer.compare(value, other.value);
+    if (comparison == 0) {
+      if (blackjack && !other.blackjack) {
+        comparison = 1;
+      } else if (other.blackjack && !blackjack) {
+        comparison = -1;
+      }
+    }
+    return comparison;
+  }
+
+  protected boolean isSoft() {
+    return soft;
+  }
+
+  public boolean isBlackjack() {
+    return blackjack;
+  }
+
+  public boolean isBusted() {
+    return busted;
+  }
+
+  public int getValue() {
     return value;
+  }
+
+  public Card[] getHand() {
+    return hand.toArray(new Card[hand.size()]);
+  }
+
+  public abstract void play() throws InsufficientCardsException;
+
+  @Override
+  public String toString() {
+    String status;
+    if (isBusted()) {
+      status = "(busted)";
+    } else if (isBlackjack()) {
+      status = "(Blackjack!)";
+    } else {
+      status = String.format("%d", getValue());
+    }
+    return String.format("Current hand: %s (%s)", Arrays.toString(getHand()), status);
   }
 }
