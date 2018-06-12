89d936579dce725d03d63887716276e77e194a21
Jay T <torrcorp@gmail.com>
2018-06-12 12:05:18 -0600

    added BlackjackHand class. - incomplete

---
+++ src/edu/cnm/deepdive/cards/BlackjackHand.java
@@ -0,0 +1,50 @@
+package edu.cnm.deepdive.cards;
+
+import edu.cnm.deepdive.cards.Deck.InsufficientCardsException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlackjackHand {
+
+  private static final int[] VALUES = {
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      10,
+      10,
+      10,
+  };
+  private List<Card> hand;
+  private Deck deck;
+
+  public BlackjackHand(Deck deck) throws InsufficientCardsException {
+    hand = new LinkedList<>();
+    hand.addAll(Arrays.asList(deck.deal(2)));
+    this.deck = deck;
+  }
+
+  private int value() {
+    boolean aceInHand = false;
+    int value = 0;
+    for (Card card : hand) {
+      if (card.getRank() == Rank.ACE) {
+        aceInHand = true;
+      }
+      value += VALUES[card.getRank().ordinal()];
+    }
+    if (value > 21) {
+      value = 0;
+    } else if (value <= 11 && aceInHand) {
+      value += 10;
+    }
+    return value;
+  }
+}
